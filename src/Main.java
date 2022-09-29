4aa27b7c6cd78a7a0deec8368d20ab301fdc8e54
Алекс <chu@mail.ru>
2022-09-29 13:48:30 +0300

    lesson5task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
+        int age1 = 15;
+        if (age1 >= 18)
+            System.out.println("Поздавление с совершенолетем");
+        if (age1 < 18)
+            System.out.println("Нужно немного подождать");}
+
+        static {int age2 = 7;
+        if (age2 >= 7)
+            System.out.println("Ребенок ходит в школку");
+
+        int age3 = 20;
+        if (age3 >= 18)
+            System.out.println("человек окончил школу и может отправляться в университет");
+
+        int age4 = 24;
+        if (age4 >= 24)
+            System.out.println("тебе пора искать работу");}
     }
-}
