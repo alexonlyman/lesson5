b7344db03d430b989c3f6e78477937664462ed18
Алекс <chu@mail.ru>
2022-09-29 14:36:11 +0300

    lesson5task2

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,44 @@
 public class Main {
     public static void main(String[] args) {
+        int capacity = 102;
+        int standingSits = 42;
+        int sedentarySits = 60;
+        if (capacity >= 102) {
+            System.out.println(" Мест нет");
+        } else {
+            System.out.println(" места есть");
+        }
+        if (sedentarySits >= 60) {
+            System.out.println(" сидячих мест нет");
+        } else {
+            System.out.println(" есть сидячие места ");
+        }
+        if (standingSits >= 42) {System.out.println(" стоячих мест нет ");}
+        else {System.out.println(" стоячие места есть"); }
 
-        int age1 = 15;
-        if (age1 >= 18)
-            System.out.println("Поздавление с совершенолетем");
-        if (age1 < 18)
-            System.out.println("Нужно немного подождать");}
 
-        static {int age2 = 7;
-        if (age2 >= 7)
-            System.out.println("Ребенок ходит в школку");
 
-        int age3 = 20;
-        if (age3 >= 18)
-            System.out.println("человек окончил школу и может отправляться в университет");
 
-        int age4 = 24;
-        if (age4 >= 24)
-            System.out.println("тебе пора искать работу");}
+            int age = 7;
+        if (age >= 7 ) {System.out.println("ребенок ходит в школу");}
+        else {System.out.println(" не ходит"); }
+
+        int age1 = 18;
+        if (age1 >= 18 ) {System.out.println("можно поступать в универ");}
+        else {System.out.println(" рано для поступления"); }
+
+        int age2 = 24;
+        if (age2 >= 24 ) {System.out.println("пора искать работу");}
+        else {System.out.println(" с работой можно пододать"); }
+
+
+
+
+
+
+
+
+
     }
+
+}
